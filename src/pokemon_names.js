74300b630b01537578d0b75d42c7f4db5ee75739
ls1955 <xxlsxx1955@gmail.com>
2023-12-23 21:24:23 +0800

    Include pokemon_names
    At last, author settled on using the provided pokeApi from The Odin
    Project to make his life easier.

---
+++ src/pokemon_names.js
@@ -0,0 +1,16 @@
+const pokemonNames = [
+  "eevee",
+  "jolteon",
+  "espeon",
+  "vaporeon",
+  "flareon",
+  "squirtle",
+  "umbreon",
+  "leafeon",
+  "totodile",
+  "sylveon",
+  "glaceon",
+  "kakuna",
+];
+
+export default pokemonNames;
