d606b6f923ac1f533bc0963f56e015d8691ecb1c
ls1955 <xxlsxx1955@gmail.com>
2023-12-24 19:46:38 +0800

    Include GameOverDialog

---
+++ src/GameOverDialog.jsx
@@ -0,0 +1,13 @@
+import "./GameOverDialog.css"
+
+function GameOverDialog() {
+  const handleRefresh = () => location.reload();
+  return (
+    <div id="game-over-dialog">
+      <h2>Congratulation, you have beat the game.</h2>
+      <button onClick={handleRefresh}>One more round?</button>
+    </div>
+  );
+}
+
+export default GameOverDialog;
