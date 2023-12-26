31bccc927e7b0b8f979e5494ce725a63eaa141c6
ls1955 <xxlsxx1955@gmail.com>
2023-12-26 20:48:04 +0800

    Include missing newline

--- src/GameOverDialog.jsx
+++ src/GameOverDialog.jsx
@@ -2,6 +2,7 @@ import "./GameOverDialog.css"
 
 function GameOverDialog() {
   const handleRefresh = () => location.reload();
+
   return (
     <div id="game-over-dialog">
       <h2>Congratulation, you have beat the game.</h2>
